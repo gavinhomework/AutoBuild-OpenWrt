38d2ae1b216fc273ef569dbcee06ad73ca0930fd
gavinhomework <92965736+gavinhomework@users.noreply.github.com>
2023-02-12 08:30:41 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,3 +17,10 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 ########### 更改大雕源码（可选）###########
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' openwrt/target/linux/x86/Makefile
+
+echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git bypass https://github.com/tianiue/luci-app-bypass' >>feeds.conf.default
+        
