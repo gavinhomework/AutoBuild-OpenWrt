fab4e4a19d7034732c8e0e1fbbaaf29eef98a57e
gavinhomework <92965736+gavinhomework@users.noreply.github.com>
2023-02-14 14:22:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -19,8 +19,5 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' openwrt/target/linux/x86/Makefile
 
 #echo "src-git ssrp https://github.com/fw876/helloworld.git" >> openwrt/feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> openwrt/feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >> openwrt/feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >> openwrt/feeds.conf.default
-echo 'src-git bypass https://github.com/tianiue/luci-app-bypass' >> openwrt/feeds.conf.default
+
         
